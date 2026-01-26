fc4b8ba1b292fad19bdc267946bc35c5388098e0
Chloe Becker <chloechina04@aol.com>
2026-01-26 10:24:57 -0500

    updated game content

--- Games/js/js.js
+++ Games/js/js.js
@@ -1,12 +1,14 @@
-fetch("../../frame/html/index.html")
-    .then(response => {
-        if (!response.ok) {
-            throw new Error("HTTP error " + response.status);
-        }
-        return response.text();
+document.addEventListener("frame:ready", () => {
+  fetch("./content.html")
+    .then(res => {
+      console.log("Fetch response:", res);
+      if (!res.ok) throw new Error("Not OK");
+      return res.text();
     })
-    .then(html => {
-        document.body.insertAdjacentHTML("afterbegin", html);
+    .then(content => {
+      document
+        .getElementById("content")
+        .insertAdjacentHTML("beforeend", content);
     })
-    .catch(err => console.error("Frame load failed:", err));
-
+    .catch(err => console.error("CONTENT LOAD FAILED:", err));
+});
