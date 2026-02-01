5f7e86576387097d6db1db8c11a36684a0464bb2
Grayhood114 <Graysonzrelak@gmail.com>
2026-02-01 13:56:41 -0500

    Settings page and assets deployed. Frame of settings page now works.

--- Setting/js/js.js
+++ Setting/js/js.js
@@ -1,16 +1,3 @@
-document.addEventListener("frame:ready", () => {
-  fetch("./content.html")
-    .then(res => {
-      console.log("Fetch response:", res);
-      if (!res.ok) throw new Error("Not OK");
-      return res.text();
-    })
-    .then(content => {
-      document
-        .getElementById("content")
-        .insertAdjacentHTML("beforeend", content);
-    })
-    .catch(err => console.error("CONTENT LOAD FAILED:", err));
-});
-
-
+function goBack() {
+  window.history.back();
+}
